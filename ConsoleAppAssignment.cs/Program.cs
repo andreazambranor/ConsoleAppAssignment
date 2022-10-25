9f026e8e9e41ce481e97b73e569359dc20621cc3
andreazambranor <azambranor.23@gmail.com>
2022-10-25 17:55:31 -0400

    adding while loop

--- ConsoleAppAssignment.cs/Program.cs
+++ ConsoleAppAssignment.cs/Program.cs
@@ -10,45 +10,63 @@ namespace ConsoleAppAssignment.cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess my age?");
-            int number = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed = number == 28;
 
-            do
-            {
-                switch (number)
-                {
-                    case 40:
-                        Console.WriteLine("I'm not that old! Try again");
-                        Console.WriteLine("Guess my age?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 20:
-                        Console.WriteLine("I'm not that young! Try again");
-                        Console.WriteLine("Guess my age?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 30:
-                        Console.WriteLine("You are close! Try again");
-                        Console.WriteLine("Guess my age?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 28:
-                        Console.WriteLine("Yes! I am 28 years old.");
-                        isGuessed = true;
-                        break;
-                    default:
-                        Console.WriteLine("You are wrong");
-                        Console.WriteLine("Guess a number?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                }
+            ////INITIAL QUESTION START BOOL
+            //Console.WriteLine("Guess my age?");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool isGuessed = number == 29;
+
+            //// DO WHILE STATEMENT
+            //do
+            //{
+            //    switch (number)
+            //    {
+            //        //CASE 1 FALSE
+            //        case 40:
+            //            Console.WriteLine("I'm not that old! Try again");
+            //            Console.WriteLine("Guess my age?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        //CASE 2 FALSE
+            //        case 20:
+            //            Console.WriteLine("I'm not that young! Try again");
+            //            Console.WriteLine("Guess my age?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        //CASE 3 FALSE
+            //        case 30:
+            //            Console.WriteLine("You are close! Try again");
+            //            Console.WriteLine("Guess my age?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        //CASE 4 TRUE
+            //        case 29:
+            //            Console.WriteLine("Yes! I am 29 years old.");
+            //            isGuessed = true;
+            //            break;
+            //        //DEFAULT FALSE
+            //        default:
+            //            Console.WriteLine("You are wrong");
+            //            Console.WriteLine("Guess my age?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
                                         
-            }
+            //}
 
-            while (!isGuessed);
-            Console.Read();
+            //while (!isGuessed);
+            //Console.Read();
+
+            Console.WriteLine("Write a random number from 1 to 20");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            while (Num < 20)
+            {
+                Console.WriteLine(Num);
+                Num++;
+            }
+            Console.ReadLine();
 
         }
+
     }
 }
